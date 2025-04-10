3290c48810a3f9dfcc51a9c58ba1aa1a0900a526
ReziBisma <bismarezi@gmail.com>
2025-04-10 10:20:18 +0700

    Upload file awal

---
+++ app/Views/v_keranjang.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+<br>“Ini halaman keranjang”<br>
+<a href="/">ini halaman home</a><br>
+<a href="/produk">ke halaman produk</a><br>
+<?= $this->endSection() ?>
---
+++ app/Views/v_produk.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+<br>ini halaman produk<br>
+<a href="/">ini halaman home</a><br>
+<a href="/keranjang">ke halaman keranjang</a>
+<?= $this->endSection() ?>
